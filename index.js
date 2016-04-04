39b3ffd517c29dd9345c075bc57c5832c233d34f
Huy Trinh <trinhphandinhhuy@gmail.com>
2016-04-04 08:20:09 +0300

    Commit from Asus 4/4

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ var app = express();
 
 var port = 3000;
 
-app.use
+app.use(express.static('public'));
+app.set('views', './src/views');
+
+app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.send('Lan va Diep');
+  res.render('index', {title: 'Hello from the other side', nav : ['Books', 'Authors']});
 });
 app.listen(port, function (err) {
   console.log('Running server on port : ' + port);
